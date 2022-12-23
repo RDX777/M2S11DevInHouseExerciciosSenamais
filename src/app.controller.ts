5ec8e057adf7ecce8888cc6598ae6610fc5c1261
Anderson Tome <anderson_tome@outlook.com.br>
2022-12-22 21:31:04 -0300

    exercicio7

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { GoogleOAuthGuard } from './core/auth/guards/google-oauth.guard';
 
 @Controller()
 export class AppController {
+
+  @Get('/auth/with-google')
+  @UseGuards(GoogleOAuthGuard)
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async googleAuth(@Request() req) {
+  }
+
+  @Get('/auth/google-redirect')
+  @UseGuards(GoogleOAuthGuard)
+  googleAuthRedirect(@Request() req) {
+    if (!req.user) {
+      return 'Sem usuário retornado do google';
+    }
+
+    return {
+      mensagem: 'Google Info',
+      user: req.user
+    }
+  }
 
 }
